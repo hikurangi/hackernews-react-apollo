6384e2d6fd697fd88a0cbc53925d90760364f216
hikurangi <hikurangi@gmail.com>
2017-11-20 15:37:04 +1300

    add Login component/path to routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Header from './Header'
+import Login from './Login'
 import LinkList from './LinkList'
 import CreateLink from './CreateLink'
 
@@ -11,6 +12,7 @@ class App extends Component {
         <Header />
         <div className='ph3 pv1 background-gray'>
           <Switch>
+            <Route exact path='/login' component={Login}/>
             <Route exact path='/' component={LinkList}/>
             <Route exact path='/create' component={CreateLink}/>
           </Switch>

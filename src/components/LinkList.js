b9712167a8683eed03c17abb7b933c60507b957d
hikurangi <hikurangi@gmail.com>
2017-11-20 17:11:37 +1300

    add _updateCacheAfterVote method and make it available in the rendered Links

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -5,6 +5,18 @@ import gql from 'graphql-tag'
 
 class LinkList extends Component {
 
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    // 1
+    const data = store.readQuery({ query: ALL_LINKS_QUERY })
+
+    // 2
+    const votedLink = data.allLinks.find(link => link.id === linkId)
+    votedLink.votes = createVote.link.votes
+
+    // 3
+    store.writeQuery({ query: ALL_LINKS_QUERY, data })
+  }
+
   render() {
 
     // 1
@@ -23,7 +35,11 @@ class LinkList extends Component {
     return (
       <div>
         {linksToRender.map((link, index) => (
-          <Link key={link.id} index={index} link={link}/>
+          <Link
+            key={link.id} updateStoreAfterVote={this._updateCacheAfterVote} index={index}
+            link={link}
+          />
+
         ))}
       </div>
     )

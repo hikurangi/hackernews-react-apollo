e75bb00828a1e5848c9c8b05a97d6b789f3ce62e
hikurangi <hikurangi@gmail.com>
2017-11-20 16:42:19 +1300

    add Apollo Link middleware to allow authentication via token

--- src/index.js
+++ src/index.js
@@ -10,13 +10,27 @@ import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
-
+import { GC_AUTH_TOKEN } from './constants'
+import { ApolloLink } from 'apollo-client-preset'
 // 2
 const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cja7gfp1i0hta0164bn0uomy0' })
 
+const middlewareAuthLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(GC_AUTH_TOKEN)
+  const authorizationHeader = token ? `Bearer ${token}` : null
+  operation.setContext({
+    headers: {
+      authorization: authorizationHeader
+    }
+  })
+  return forward(operation)
+})
+
+const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink)
+
 // 3
 const client = new ApolloClient({
-  link: httpLink,
+  link: httpLinkWithAuthToken,
   cache: new InMemoryCache()
 })
 

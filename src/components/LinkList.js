ec26cb01a47d7b88c829a6527095b76689dacdc1
hikurangi <hikurangi@gmail.com>
2017-11-20 14:22:16 +1300

    create LinkList generator component

---
+++ src/components/LinkList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import Link from './Link'
+
+class LinkList extends Component {
+
+  render() {
+
+    const linksToRender = [{
+      id: '1',
+      description: 'The Coolest GraphQL Backend 😎',
+      url: 'https://www.graph.cool'
+    }, {
+      id: '2',
+      description: 'The Best GraphQL Client',
+      url: 'http://dev.apollodata.com/'
+    }]
+
+    return (
+      <div>
+        {linksToRender.map(link => (
+          <Link key={link.id} link={link}/>
+        ))}
+      </div>
+    )
+  }
+
+}
+
+export default LinkList

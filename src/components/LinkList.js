5e1b81a475342df9b385f99bc8074875e271f3fa
hikurangi <hikurangi@gmail.com>
2017-11-20 14:44:04 +1300

    add clarification

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -45,4 +45,4 @@ const ALL_LINKS_QUERY = gql`
 `
 
 // 3
-export default graphql(ALL_LINKS_QUERY, { name: 'allLinksQuery' }) (LinkList) // graphql wraps the LinkList here, providing props.allLinksQuery from the store
+export default graphql(ALL_LINKS_QUERY, { name: 'allLinksQuery' }) (LinkList) // graphql wraps the LinkList in this IIFE, providing props.allLinksQuery from the store

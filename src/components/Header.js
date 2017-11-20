dced4edb77a304f380a78d2936b0404ee992fcbb
hikurangi <hikurangi@gmail.com>
2017-11-20 18:01:33 +1300

    add '/top' route

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,6 +13,8 @@ class Header extends Component {
           <div className='fw7 mr1'>Hacker News</div>
           <Link to='/' className='ml1 no-underline black'>new</Link>
           <div className='ml1'>|</div>
+          <Link to='/top' className='ml1 no-underline black'>top</Link>
+          <div className='ml1'>|</div>
           <Link to='/search' className='ml1 no-underline black'>search</Link>
           {userId &&
           <div className='flex'>

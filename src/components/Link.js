79b8c6da5e9149baceb19dcfefa77abd39c13d0c
hikurangi <hikurangi@gmail.com>
2017-11-20 16:46:18 +1300

    flesh out Link component, add GC_USER_ID token and timeDifferenceForDate helper function

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
+import { GC_USER_ID } from '../constants'
+import { timeDifferenceForDate } from '../utils'
 
 class Link extends Component {
 
   render() {
+    const userId = localStorage.getItem(GC_USER_ID)
     return (
-      <div>
-        <div>{this.props.link.description} ({this.props.link.url})</div>
+      <div className='flex mt2 items-start'>
+        <div className='flex items-center'>
+          <span className='gray'>{this.props.index + 1}.</span>
+          {userId && <div className='ml1 gray f11' onClick={() => this._voteForLink()}>▲</div>}
+        </div>
+        <div className='ml1'>
+          <div>{this.props.link.description} ({this.props.link.url})</div>
+          <div className='f6 lh-copy gray'>{this.props.link.votes.length} votes | by {this.props.link.postedBy ? this.props.link.postedBy.name : 'Unknown'} {timeDifferenceForDate(this.props.link.createdAt)}</div>
+        </div>
       </div>
     )
   }
